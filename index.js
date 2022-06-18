db5ccd074d1f5f34f0cfd3fbbe6abf0fb13cb31c
Agustin De Rose <agustinderose@MBP-de-Agustin.fibertel.com.ar>
2022-06-18 16:41:05 -0300

    Changes in Agustin Landing- social media icons

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-// $(function () {
-//     $('[data-bs-toggle="tooltip"]').tooltip()
-// })
-
-
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
